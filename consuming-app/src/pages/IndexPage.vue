4e918161b37502c1e32ec59e4dadeb5066ea8e5f
Greg <greg.bengis@gmail.com>
2024-07-20 12:21:10 +0200

    'message'

---
+++ consuming-app/src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    Hello
+
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script setup>
+import {compo} from "quasar-app-extension-my-component";
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
---
+++ my-component/src/boot/register-my-component.js
@@ -0,0 +1,6 @@
+import MyComponent from '../component/MyComponent.vue'
+
+export default ({ app }) => {
+  // we globally register our component in the app
+  app.component('compo', MyComponent)
+}
